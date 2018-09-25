829bcac9cb53e7800a09a1c5153de756f3dd1ab5
Justin Shenk <shenkjustin@gmail.com>
2018-09-25 22:15:40 +0200

    [WIP] Update routing, etc.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import base64
 import glob
+import time
 import urllib
 
 import cv2
@@ -14,7 +15,8 @@ import seaborn as sns
 
 from fer.fer import FER
 from fer.classes import Video
-from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory, session
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory, session, \
+    make_response, jsonify
 import keras.backend as K
 from werkzeug.utils import secure_filename
 
@@ -22,7 +24,7 @@ app = Flask(__name__)
 app.secret_key = b'secret_key'
 UPLOAD_FOLDER = os.path.join(app.instance_path, 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 30 * 1024 * 1024 # 30 MB limit for 1 day
+app.config['MAX_CONTENT_LENGTH'] = 30 * 1024 * 1024 # 30 MB limit
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 current_df = pd.DataFrame()
 SAMPLE_FILE = os.path.join(app.config['UPLOAD_FOLDER'], "test.mp4")
@@ -106,12 +108,14 @@ def get_plots(columns, overlay=False):
     app.logger.info(plots)
     return plots
 
+def to_uploads(filename):
+    return os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
 def load_video(filename):
     global current_video
     current_video = Video(filename,
                           outdir=app.config['UPLOAD_FOLDER'],
-                          tempfile=os.path.join(app.config['UPLOAD_FOLDER'],'temp_outfile.mp4'))
+                          tempfile=to_uploads('temp_outfile.mp4'))
     return current_video
 
 def get_frame(video_obj, frame_nr=0, encoding = 'base64'):
@@ -123,50 +127,13 @@ def get_frame(video_obj, frame_nr=0, encoding = 'base64'):
     video_obj.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
     h, w = frame.shape[:2]
     frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-    if encoding is 'base64':
+    if encoding is 'opencv':
+        return frame
+    elif encoding is 'base64':
         retval, buffer = cv2.imencode('.jpg', frame)
         b64image = base64.b64encode(buffer)
         data_url = b'data:image/jpeg;base64,' + b64image
         return data_url.decode('utf8')
-    return frame
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-
-@app.route('/upload', methods=['GET','POST'])
-def upload():
-    global current_df, current_video, graph
-    output = {}
-    app.logger.info("/upload accessed")
-    if request.method == 'POST':
-        # check if the post request has the file part
-        file = request.files.get('file')
-        app.logger.info("{} - File received".format(file.filename))
-        if not file:
-            flash('No file part', category="Error")
-            app.logger.info('No file part')
-            return redirect(request.url)
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file', category="Error")
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            mp4path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(mp4path)
-            app.logger.info("Saved to {}".format(mp4path))
-            current_video = load_video(mp4path)
-            screenshot = get_frame(current_video, encoding = 'base64')
-            output['screenshot'] = screenshot
-            # current_df = display_file(mp4path)
-            output['dataframe'] = current_df.head(1).to_html(float_format=lambda x: '%.2f' % x)
-            session['filename'] = filename
-            session['loaded'] = True
-    return render_template('index.html', **output, **session)
-
 
 def get_output_images(outdir, nr=3):
     files = glob.glob(os.path.join(outdir,'frame*.jpg'))
@@ -182,54 +149,113 @@ def get_output_images(outdir, nr=3):
             break
     return output_images
 
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    global current_df, current_video, graph
+    app.logger.info("/upload accessed")
+    if request.method == 'POST':
+        # check if the post request has the file part
+        file = request.files.get('file')
+        app.logger.info("{} - File received".format(file.filename))
+        if not file:
+            flash('No file part', category="Error")
+            app.logger.error('No file part')
+            return redirect(request.url)
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file', category="Error")
+            return redirect(request.url)
+        elif file and allowed_file(file.filename):
+            session.clear()
+            filename = secure_filename(file.filename)
+            mp4path = to_uploads(filename)
+            file.save(mp4path)
+            app.logger.info("Saved to {}".format(mp4path))
+            current_video = load_video(mp4path)
+            screenshot = get_frame(current_video, encoding='opencv')
+            cv2.imwrite(to_uploads('screenshot.png'), screenshot)
+            session['screenshot'] = 'screenshot.png'
+            session['filename'] = filename
+            session['loaded'] = True
+    return redirect(url_for('analyze'))
+
+@app.route('/analyze', methods=['GET'])
+def analyze():
+    global current_df, current_video, graph
+    output = {}
+    session['explore'] = True
+    # Remove previous frames
+    [os.remove(f) for f in glob.glob(to_uploads('frame*.jpg'))]
+    # Analyze video and save every 50th frame
+    with graph.as_default():
+        print("analyzing in server")
+        raw_data = current_video.analyze(detector, display=False, frequency=30)
+    video_outfile = 'output.mp4'
+    assert os.path.isfile(to_uploads(video_outfile)), app.logger.error("no output.mp4")
+    session['video_filename'] = 'output.mp4'
+    df = current_video.to_pandas(raw_data)
+    current_df = current_video.get_first_face(df).dropna()
+    csvpath = ''.join(session['filename'].split('.')[:-1]) + '.csv'
+    csvpath = to_uploads(csvpath)
+    current_df.to_csv(csvpath)
+    output['csv_filename'] = os.path.split(csvpath)[1]
+    output['dataframe'] = current_df.head(10).to_html(float_format=lambda x: '%.2f' % x)
+    output['output_images'] = get_output_images(current_video.outdir)
+    emotions = current_video.get_emotions(current_df)
+    emotions.plot()
+    emotions_chart = to_uploads('emotions_chart.png')
+    plt.savefig(emotions_chart)
+    output['emotions_chart'] = 'emotions_chart.png'
+    return render_template('results.html', **output, **session)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global current_df, current_video, graph
     output = {}
-    if request.args.get('action') == 'reset':
-        session.clear()
-        return redirect(url_for('index'))
     # Load and display sample file
     if request.args.get('action') == 'load_sample':
-        app.logger.info("loading sample")
         filename = SAMPLE_FILE.split('/')[-1]
         current_video = load_video(SAMPLE_FILE)
-        screenshot = get_frame(current_video, encoding = 'base64')
-        output['screenshot'] = screenshot
+        screenshot = get_frame(current_video, encoding='opencv')
+        cv2.imwrite(to_uploads('screenshot.png'), screenshot)
+        session['screenshot'] = 'screenshot.png'
         session['filename'] = filename
         session['loaded'] = True
         session.pop('explore', None)
-    if request.args.get('action') == 'analyze':
-        session['explore'] = True
-        # Remove previous frames
-        [os.remove(f) for f in glob.glob(os.path.join(app.config['UPLOAD_FOLDER'],'frame*.jpg'))]
-
-        # Analyze video and save every 50th frame
-        with graph.as_default():
-            raw_data = current_video.analyze(detector, display=False, frequency=30)
-        video_outfile = 'output.mp4'
-        assert os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], video_outfile)), app.logger.error("no output.mp4")
-        session['video_filename'] = 'output.mp4'
-        df = current_video.to_pandas(raw_data)
-        current_df = current_video.get_first_face(df).dropna()
-        csvpath = ''.join(session['filename'].split('.')[:-1]) + '.csv'
-        csvpath = os.path.join(app.config['UPLOAD_FOLDER'], csvpath)
-        current_df.to_csv(csvpath)
-        output['csv_filename'] = os.path.split(csvpath)[1]
-        output['dataframe'] = current_df.head(10).to_html(float_format=lambda x: '%.2f' % x)
-        output['output_images'] = get_output_images(current_video.outdir)
-        emotions = current_video.get_emotions(current_df)
-        emotions.plot()
-        emotions_chart = os.path.join(app.config['UPLOAD_FOLDER'], 'emotions_chart.png')
-        plt.savefig(emotions_chart)
-        output['emotions_chart'] = 'emotions_chart.png'
-    # if request.args.get('hist_plot'):
-    #     overlay = request.args.get('overlayCheck')
-    #     columns = request.args.getlist('hist_plot')
-    #     app.logger.info(columns)
-    #     session['analysis'] = 'hist_plot'
-    #     output['plots'] = get_plots(columns, overlay)
+    # if request.args.get('action') == 'analyze':
+    #     session['explore'] = True
+    #     # Remove previous frames
+    #     [os.remove(f) for f in glob.glob(to_uploads('frame*.jpg'))]
+    #     # Analyze video and save every 50th frame
+    #     with graph.as_default():
+    #         raw_data = current_video.analyze(detector, display=False, frequency=30)
+    #     video_outfile = 'output.mp4'
+    #     assert os.path.isfile(to_uploads(video_outfile)), app.logger.error("no output.mp4")
+    #     session['video_filename'] = 'output.mp4'
+    #     df = current_video.to_pandas(raw_data)
+    #     current_df = current_video.get_first_face(df).dropna()
+    #     csvpath = ''.join(session['filename'].split('.')[:-1]) + '.csv'
+    #     csvpath = to_uploads(csvpath)
+    #     current_df.to_csv(csvpath)
+    #     output['csv_filename'] = os.path.split(csvpath)[1]
+    #     output['dataframe'] = current_df.head(10).to_html(float_format=lambda x: '%.2f' % x)
+    #     output['output_images'] = get_output_images(current_video.outdir)
+    #     emotions = current_video.get_emotions(current_df)
+    #     emotions.plot()
+    #     emotions_chart = to_uploads('emotions_chart.png')
+    #     plt.savefig(emotions_chart)
+    #     output['emotions_chart'] = 'emotions_chart.png'
     return render_template('index.html', **output, **session)
 
 @app.after_request
