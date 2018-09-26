69a9c763ae4552499785391221f3ae8488356fe4
Justin Shenk <shenk.justin@gmail.com>
2018-09-26 22:19:52 +0200

    Fix quotes

--- app.py
+++ app.py
@@ -285,6 +285,6 @@ def add_header(response):
 if __name__ == '__main__':
     app.run()
 else:
-    gunicorn_logger = logging.getLogger(‘gunicorn.error’)
+    gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
